96eabfde2af23c9afd3d5f89242bc70f44b8e27e
Gustavo Henrique Michels <ghmichels@gmail.com>
2019-09-12 17:17:35 -0300

    Add notifications and profile crud

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Endereço de e-mail" />
+
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua nova senha"
+        />
+        <hr />
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -13,11 +16,16 @@ const schema = Yup.object().shape({
   password: Yup.string()
     .min(6, 'A senha deve ter no mínimo 6 caracteres.')
     .required('A senha é obrigatória.'),
+  confirmPassword: Yup.string()
+    .min(6, 'A senha deve ter no mínimo 6 caracteres.')
+    .required('A confirmação de senha é obrigatória.'),
 });
 
 export default function SignUp() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password, confirmPassword }) {
+    dispatch(signUpRequest(name, email, password, confirmPassword));
   }
 
   return (
@@ -27,6 +35,11 @@ export default function SignUp() {
         <Input name="name" placeholder="Seu nome completo" />
         <Input name="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua senha"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho uma conta</Link>
